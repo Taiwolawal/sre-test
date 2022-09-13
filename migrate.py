7ff0c59cc1e828f9c7e25a2fa387ab20dd6c3569
Taiwo Lawal <taiwolawal360@gmail.com>
2022-09-13 14:26:38 +0100

    edit access key

--- migrate.py
+++ migrate.py
@@ -7,14 +7,14 @@ import psycopg2
 import boto3
 
 #database & S3 bucket
-DB_CONN_STRING = os.getenv('DB_CONN_STRING', 'postgres://postgres:mysecretpassword@database-1.cxpqgzfn1lf4.us-east-1.rds.amazonaws.com/proddatabase')
-S3_LEGACY_BUCKET = os.getenv('S3_LEGACY_BUCKET', 'legacy-s3-bucket2022')
+DB_CONN_STRING = os.getenv('DB_CONN_STRING', '<>')
+S3_LEGACY_BUCKET = os.getenv('S3_LEGACY_BUCKET', '<>')
 S3_PRODUCTION_BUCKET = os.getenv('S3_PRODUCTION_BUCKET', 'production-s3-2022')
 
 # S3 connection details
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'AKIAR27JRZKSG2VXQLMV')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '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')
-AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '<>')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '<>')
+AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION', '<>')
 
 #Connecting to S3
 try:
@@ -33,3 +33,23 @@ try:
 except Exception as e:
     logging.error(f"Error while connecting to the database: {e}")
     sys.exit(1)
+
+
+#Connecting to legacy S3 objetcs
+legacy_bucket = s3.Bucket(S3_LEGACY_BUCKET)
+
+#
+for file in legacy_bucket.objects.all():
+    new_key = file.key.split("/")
+    new_key = 'avatar/' + new_key[-1]
+    copy_source = {
+        'Bucket': file.bucket_name,
+        'Key': file.key
+    }
+    try:
+        #Copy an object from one S3 location to another
+        s3.meta.client.copy(copy_source, S3_PRODUCTION_BUCKET, new_key)
+        update_row(conn, file.key, new_key)
+        s3.Object(file.bucket_name, file.key).delete()
+    except Exception as e:
+        logging.error(f"Something went wrong: {e}")
