98fb903da63698c08d160337319016be28a00453
Taiwo Lawal <taiwolawal360@gmail.com>
2022-09-13 13:50:49 +0100

    ensure boto3 connects to S3 resources

--- migrate.py
+++ migrate.py
@@ -15,3 +15,14 @@ S3_PRODUCTION_BUCKET = os.getenv('S3_PRODUCTION_BUCKET', 'production-s3-2022')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'AKIAR27JRZKSG2VXQLMV')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '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')
 AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+
+#Try connecting to S3
+try:
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=AWS_ACCESS_KEY_ID,
+                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                        region_name=AWS_DEFAULT_REGION
+                        )
+except Exception as e:
+    logging.error(f"Error while connecting to S3: {e}")
+    sys.exit(1)
